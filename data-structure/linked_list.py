67d20fef8cc4aa4a61eb103f76cc263dca003ba8
Ukjin <asllan77@gmail.com>
2020-06-13 13:13:57 -0400

    Set default linked list to empty

--- data-structure/linked_list.py
+++ data-structure/linked_list.py
@@ -8,13 +8,17 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self, item):
-        self.head = Node(item)
-        self.tail = self.head
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
     def add(self, item):
-        self.tail.next = Node(item)
-        self.tail = self.tail.next
+        if self.tail is None:
+            self.tail = Node(item)
+            self.head = self.tail
+        else:
+            self.tail.next = Node(item)
+            self.tail = self.tail.next
 
     def remove(self, item):
         if self.head.val == item:
@@ -56,7 +60,8 @@ class LinkedList:
 
 class TestLinkedList(unittest.TestCase):
     def test(self):
-        ll = LinkedList(3)
+        ll = LinkedList()
+        ll.add(3)
         self.assertEqual(ll.head.val, 3)
         ll.add(4)
         self.assertEqual(ll.head.next.val, 4)
@@ -74,7 +79,8 @@ class TestLinkedList(unittest.TestCase):
         ll.remove(6)
         self.assertEqual(ll.head.next.val, 7)
 
-        ll2 = LinkedList(9)
+        ll2 = LinkedList()
+        ll2.add(9)
         ll2.add(8)
         ll2.add(7)
         ll2.reverse()
